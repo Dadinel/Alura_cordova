be9bd2c5940063beab200cf868909494c1ac3c0e
Daniel Mendes de Melo Sousa <daniel.mendes@sp01.local>
2018-07-23 20:47:26 -0300

    Finalizada a aula 06.

--- garconapp/www/js/app.js
+++ garconapp/www/js/app.js
@@ -1,11 +1,46 @@
 $('.collection-item').on('click', function() {
-    var $badge = $('.badge', this);
+    let $badge = $('.badge', this);
 
-    if($badge.length == 0) {
+    if($badge.length === 0) {
         $badge = $('<span class="badge brown-text">0</span>').appendTo(this);
     }
 
     $badge.text(parseInt($badge.text()) + 1);
+
+    let nomeProduto = this.firstChild.textContent;
+    Materialize.toast(nomeProduto + ' adicionado', 500);
+});
+
+$('#confirmar').on('click', function() {
+    let texto = '';
+
+    $('.badge').parent().each(function() {
+        let produto = this.firstChild.textContent;
+
+        if(this.classList.contains('full')) {
+            produto += ' inteiro';
+        }
+
+        let quantidade = this.lastChild.textContent;
+
+        texto += produto + ': ' + quantidade + ', ';
+    });
+
+    //Remove a última vírgula
+    texto = texto.trim();
+    texto = texto.substr(0, texto.length - 1 );
+
+    $('#resumo').empty().text(texto);
 });
 
 $('.modal-trigger').leanModal();
+
+$('.collection').on('click', '.badge' , function() {
+    $(this).remove();
+    return false;
+});
+
+$('.acao-limpar').on('click', function() {
+    $('#numero-mesa').val('');
+    $('.badge').remove();
+});
